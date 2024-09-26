9c07f372dde7631818e70ac5ee565f7cb428ace6
Dev avology <devavology12@gmail.com>
2024-09-26 13:29:22 +0530

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,6 +28,33 @@ document.addEventListener("DOMContentLoaded", function () {
 AOS.init();
 // AOS JS Start
 
+const loginForm = document.getElementById('login-form');
+const signupForm = document.getElementById('signup-form');
+const loginLink = document.getElementById('login-link');
+const signupLink = document.getElementById('signup-link');
+
+loginLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  signupForm.style.display = 'none';
+  loginForm.style.display = 'block';
+
+  setTimeout(() => {
+    signupForm.style.opacity = 0;
+    loginForm.style.opacity = 1;
+  }, 10);
+});
+
+signupLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  loginForm.style.display = 'none';
+  signupForm.style.display = 'block';
+
+  setTimeout(() => {
+    loginForm.style.opacity = 0;
+    signupForm.style.opacity = 1;
+  }, 10);
+});
+
 
 
 
